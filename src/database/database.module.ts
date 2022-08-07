01a2dd26b6140f7757e030e2d052f8118451b5b4
getalihassan <ali.hassan@prixite.com>
2022-08-08 01:37:06 +0500

    added db module

---
+++ src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'secret',
+      database: 'projectz',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
