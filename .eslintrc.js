0bf0fafbef8b9e2b0b70177fa07930b5553a8722
Ali Hassan <24819103+thisalihassan@users.noreply.github.com>
2022-08-09 20:05:12 +0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'eol-last': 2,
+     "no-unused-vars": 2,
     'no-trailing-spaces': 2,
   },
 };
